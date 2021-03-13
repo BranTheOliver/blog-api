cdb81ff1c905b1d81606a6d13c1701a24ed0a1a5
brantheoliver <bran.oliv22@gmail.com>
2021-03-12 23:06:30 -0300

    Feat: Add Get User by id method

--- blog/main.py
+++ blog/main.py
@@ -81,7 +81,7 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     return "Updated"
 
 
-@app.post("/user")
+@app.post("/user", response_model=schemas.UserResponse)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
     new_user = models.User(
         name=request.name, email=request.email, password=Hash().hash(request.password)
@@ -90,3 +90,16 @@ def create_user(request: schemas.User, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@app.get("/user/{id}", response_model=schemas.UserResponse)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User with id {id} is not found.",
+        )
+
+    return user
