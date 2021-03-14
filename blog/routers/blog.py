94c810c8230774961b1a099d111aebc16914b66c
brantheoliver <bran.oliv22@gmail.com>
2021-03-13 21:12:20 -0300

    Fix: Refactor http requests
    Refactor the requests to another files in crud folder

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -3,70 +3,35 @@ from sqlalchemy.orm import Session
 from typing import List
 
 from .. import schemas, models, database
+from ..crud import blog
 
-router = APIRouter()
+router = APIRouter(prefix="/blog", tags=["Blog"])
+
 get_db = database.get_db
 
 
-@router.get("/blog", response_model=List[schemas.BlogResponse], tags=["blog"])
+@router.get("/", response_model=List[schemas.BlogResponse])
 def get_all(db: Session = Depends(get_db)):
-    blogs = db.query(models.Blog).all()
-    return blogs
+    return blog.get_all(db)
 
 
 @router.get(
-    "/blog/{id}",
-    status_code=status.HTTP_200_OK,
-    response_model=schemas.BlogResponse,
-    tags=["blog"],
+    "/{id}", status_code=status.HTTP_200_OK, response_model=schemas.BlogResponse
 )
-def get_one(id, response: Response, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blog:
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {f"Blog with the id {id} is not available."}
-        # this can be replaced by:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Blog with id {id} is not available.",
-        )
-    return blog
+def get_one(id: int, response: Response, db: Session = Depends(get_db)):
+    return blog.get_one(id, db)
 
 
-@router.post("/blog", status_code=status.HTTP_201_CREATED, tags=["blog"])
+@router.post("/", status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title, body=request.body, user_id=1)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
+    return blog.create(request, db)
 
 
-@router.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT, tags=["blog"])
-def delete(id, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-
-    if not blog.first():
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Blog with id {id} is not available.",
-        )
-
-    blog.delete(synchronize_session=False)
-    db.commit()
-    return "Deleted"
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete(id: int, db: Session = Depends(get_db)):
+    return blog.delete(id, db)
 
 
-@router.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED, tags=["blog"])
-def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-
-    if not blog.first():
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Blog with id {id} is not found.",
-        )
-
-    blog.update(request)
-    db.commit()
-    return "Updated"
+@router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+    return blog.update(id, request, db)
--- blog/routers/user.py
+++ blog/routers/user.py
@@ -3,31 +3,18 @@ from sqlalchemy.orm import Session
 from typing import List
 
 from .. import schemas, models, database
-from ..hashing import Hash
+from ..crud import user
 
-router = APIRouter()
+router = APIRouter(prefix="/user", tags=["User"])
+
 get_db = database.get_db
 
 
-@router.post("/user", response_model=schemas.UserResponse, tags=["user"])
+@router.post("/", response_model=schemas.UserResponse)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
-    new_user = models.User(
-        name=request.name, email=request.email, password=Hash().hash(request.password)
-    )
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+    return user.create(request, db)
 
 
-@router.get("/user/{id}", response_model=schemas.UserResponse, tags=["user"])
+@router.get("/{id}", response_model=schemas.UserResponse)
 def get_user(id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"User with id {id} is not found.",
-        )
-
-    return user
+    return user.get_user(id, db)
